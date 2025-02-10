ee0bcea0118bf5e6957a016552a1a0fa65fb01f0
Rishi Verma <vrishi7654@gmail.com>
2025-02-10 17:05:05 +0530

    files changed

--- src/config/database.js
+++ src/config/database.js
@@ -1,2 +1,13 @@
 const mongoose = require("mongoose");
 
+const connectDb = async()=>{
+   await mongoose.connect("mongodb+srv://risshi_codes:U5rfIRbXyAoFmTQz@namastenode.hfv93.mongodb.net/devTinder");
+}
+
+
+
+module.exports = {
+    connectDb,
+}
+
+
