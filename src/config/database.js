c1e3031411d9d7aa54e11a8dbafed18ca0f52cfa
Rishi Verma <vrishi7654@gmail.com>
2025-02-10 17:42:47 +0530

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDb = async()=>{
-   await mongoose.connect("mongodb+srv://risshi_codes:U5rfIRbXyAoFmTQz@namastenode.hfv93.mongodb.net/devTinder");
+   await mongoose.connect("");
 }
 
 
